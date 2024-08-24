82dbd49bff8c3dff3deeb3233fe178bb474303bc
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-08-23 22:57:19 -0300

    feat: Implements SplashScreen

--- app/src/main/java/com/wanderlei/movieapp/MainActivity.kt
+++ app/src/main/java/com/wanderlei/movieapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.wanderlei.movieapp.core.presentation.MainScreen
 import com.wanderlei.movieapp.ui.theme.MovieAppTheme
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        installSplashScreen()
         setContent {
             MovieAppTheme {
                 MainScreen(navController = rememberNavController())
